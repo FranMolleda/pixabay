b5c0f2a35c298ce124cd5b092c13f6a01432061a
Fran <fmjerez@icloud.com>
2020-09-18 09:47:53 +0200

    Form prepared

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Form from "./components/Form";
 
 function App() {
+  //ApiKey=18342908-2d1927776b8451fa90751081b
+  //url=https://pixabay.com/api/?key=18342908-2d1927776b8451fa90751081b&q=yellow+flowers&image_type=photo
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="containner">
+      <div className="jumbotron">
+        <p className="lead text-center">Images Searching</p>
+
+        <Form />
+      </div>
     </div>
   );
 }
