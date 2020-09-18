f634eee08e74acae16e3b9af25db81c91d905327
Fran <fmjerez@icloud.com>
2020-09-18 14:03:53 +0200

    Image Listr done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Form from "./components/Form";
-
+import ImageList from "./components/ImagesList";
 function App() {
   const [search, setSearch] = useState("");
   const [listimage, setListImage] = useState([]);
@@ -17,25 +17,16 @@ function App() {
     };
     ApiConsult();
   }, [search]);
-  // useEffect(() => {
-  //   if (search === "") return;
-  //   const ApiConsult = async () => {
-  //     const url = `https://pixabay.com/api/?key=18342908-2d1927776b8451fa90751081b&q=${search}&image_type=photo`;
-  //     const response = await axios(url);
-  //     setImage(response.data.hits);
-  //   };
 
-  //   ApiConsult();
-  // }, [search]);
-
-  //ApiKey=18342908-2d1927776b8451fa90751081b
-  //url=https://pixabay.com/api/?key=18342908-2d1927776b8451fa90751081b&q=yellow+flowers&image_type=photo
   return (
     <div className="containner">
       <div className="jumbotron">
         <p className="lead text-center">Images Searching</p>
 
         <Form setSearch={setSearch} />
+      </div>
+      <div className="row justify-content-center">
+        <ImageList listimage={listimage} />
       </div>
     </div>
   );
